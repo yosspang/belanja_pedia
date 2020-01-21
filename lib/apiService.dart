4b921f6411ac0912cdab93ba225d19bf0e9fccd8
Yosua Simatupang <yosua@dkatalis.com>
2020-01-21 14:42:10 +0700

    implement  get data from api using dio

---
+++ lib/apiService.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+class ApiService {
+  hello() async {
+    Dio dio = new Dio();
+    return await dio.get("http://trying-travis1.herokuapp.com/").then((res) {
+      print(res.data);
+      return res.data;
+    }).catchError((err) => err);
+  }
+}
