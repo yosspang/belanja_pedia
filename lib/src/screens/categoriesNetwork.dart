4f7849f4532899306142a6a6af310d82ff2b1dd6
shanon3 <shanonathooya@gmail.com>
2020-01-31 08:34:12 +0700

    filter categories on progress

---
+++ lib/src/screens/categoriesNetwork.dart
@@ -0,0 +1,26 @@
+import 'package:belanja_pedia/src/bloc/productsBloc.dart';
+import 'package:belanja_pedia/src/model/product.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/product_row_item.dart';
+
+class ProductListTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ProductsBloc productsBloc = ProductsBloc();
+    Stream products = productsBloc.productsList;
+
+    return StreamBuilder<List<Products>>(
+      stream: products,
+      builder: (context, snapshot) {
+        if(snapshot.hasError) print(snapshot.error);
+        return snapshot.hasData 
+          ? new ProductRowItem(product: snapshot.data, length: snapshot.data.length)
+          : new Center(child: CircularProgressIndicator(
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.green),
+          ));
+      },
+    );
+  }
+}
